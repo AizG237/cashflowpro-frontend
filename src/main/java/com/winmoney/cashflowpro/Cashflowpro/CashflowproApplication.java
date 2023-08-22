dd166e89ee0467fa65f9b86629a1138b44c70166
Fono Yannick <yfono123@gmail.com>
2023-08-22 04:03:17 +0100

    first backend commit

---
+++ src/main/java/com/winmoney/cashflowpro/Cashflowpro/CashflowproApplication.java
@@ -0,0 +1,13 @@
+package com.winmoney.cashflowpro.Cashflowpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashflowproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashflowproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/winmoney/cashflowpro/Cashflowpro/CashflowproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.winmoney.cashflowpro.Cashflowpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashflowproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
